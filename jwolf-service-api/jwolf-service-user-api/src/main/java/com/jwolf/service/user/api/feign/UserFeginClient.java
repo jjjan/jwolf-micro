1b9e42a72e1c53ac950faedceb3e845608236cd3
majun <523083921@qq.com>
2021-11-14 14:28:00 +0800

    通过naocs注册中心服务调用

--- jwolf-service-api/jwolf-service-user-api/src/main/java/com/jwolf/service/user/api/feign/UserFeginClient.java
+++ jwolf-service-api/jwolf-service-user-api/src/main/java/com/jwolf/service/user/api/feign/UserFeginClient.java
@@ -3,7 +3,9 @@ package com.jwolf.service.user.api.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient(value = "user",url = "http://localhost:8881",fallback = UserFeginClientFallback.class)
+//指定url直接调用 不走注册中心
+//@FeignClient(value = "user",url = "http://localhost:8881",fallback = UserFeginClientFallback.class)
+@FeignClient(value = "user"   ,fallback = UserFeginClientFallback.class)
 public interface UserFeginClient {
     @GetMapping(value = "/user/user/feign")
     String getUserByFeign( @RequestParam("id") long id);
