2f01e90ea93f88f8a348b940e6315382398f47ed
majun <523083921@qq.com>
2021-11-17 00:35:21 +0800

    网关全局异常处理器。各下游服务器就不用设置全局异常处理了，有网关统一处理就够了，避免sentinel感知不到异常无法触发相关规则，如果不处理，则会抛出流控降级异常到前端

---
+++ jwolf-gateway/src/main/java/com/jwolf/jwolfgateway/config/GatewaySentinelConfiguration.java
@@ -0,0 +1,95 @@
+package com.jwolf.jwolfgateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.SentinelGatewayConstants;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPathPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayParamFlowItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 网关限流配置
+ */
+@Configuration
+public class GatewaySentinelConfiguration {
+ 
+
+
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
+    }
+ 
+    @PostConstruct
+    public void doInit() {
+       // initSystemRule();
+        initCustomizedApis();
+        initGatewayRules();
+    }
+ 
+    private void initCustomizedApis() {
+        Set<ApiDefinition> definitions = new HashSet<>();
+        ApiDefinition api1 = new ApiDefinition("user_api")
+                .setPredicateItems(new HashSet<ApiPredicateItem>() {{
+                    add(new ApiPathPredicateItem().setPattern("/jwolf/user/**")
+                            .setMatchStrategy(SentinelGatewayConstants.URL_MATCH_STRATEGY_PREFIX));
+                }});
+        ApiDefinition api2 = new ApiDefinition("travel_api")
+                .setPredicateItems(new HashSet<ApiPredicateItem>() {{
+                    add(new ApiPathPredicateItem().setPattern("/jwolf/travel/**")
+                            .setMatchStrategy(SentinelGatewayConstants.URL_MATCH_STRATEGY_PREFIX));
+                }});
+        definitions.add(api1);
+        definitions.add(api2);
+        GatewayApiDefinitionManager.loadApiDefinitions(definitions);
+    }
+ 
+    /**
+     *
+     *
+     * 自定义网关限流规则（反爬虫机制）
+     * 1.对所有api接口通过IP进行限流,每个IP，1秒钟内请求数量大于2，即视为爬虫
+     * 2.对小说内容接口访问进行限流，每个IP，1秒钟请求数量大于1，则视为爬虫
+     *
+     * */
+    private void initGatewayRules() {
+        Set<GatewayFlowRule> rules = new HashSet<>();
+        rules.add(new GatewayFlowRule("user_api")
+                .setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_CUSTOM_API_NAME)
+                .setCount(2)
+                .setIntervalSec(1)
+                .setParamItem(new GatewayParamFlowItem()
+                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_CLIENT_IP)
+                )
+        );
+        rules.add(new GatewayFlowRule("travel_api")
+                .setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_CUSTOM_API_NAME)
+                .setCount(1)
+                .setIntervalSec(1)
+                .setParamItem(new GatewayParamFlowItem()
+                        .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_CLIENT_IP)
+                )
+        );
+        GatewayRuleManager.loadRules(rules);
+    }
+}
---
+++ jwolf-gateway/src/main/java/com/jwolf/jwolfgateway/config/GatewaySentinelGlobleExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.jwolf.jwolfgateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.fastjson.JSON;
+import com.jwolf.common.base.entity.ResultEntity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebExceptionHandler;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 相当于自定义全局异常处理器。各下游服务器就不用设置全局异常处理了，有网关统一处理就够了，避免sentinel感知不到异常无法触发相关规则
+ * 如果不处理，则会抛出流控降级异常到前端
+ */
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class GatewaySentinelGlobleExceptionHandler implements WebExceptionHandler {
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        if (exchange.getResponse().isCommitted()) {
+            return Mono.error(ex);
+        }
+        if (!BlockException.isBlockException(ex)) {
+            return writeResponse(ResultEntity.fail("服务异常,详情:" + ex.getMessage()), exchange);
+        }
+        return GatewayCallbackManager.getBlockHandler()
+                .handleRequest(exchange, ex)
+                .flatMap(response -> {
+                    //可设置响应头，httpStatus等
+                    ServerHttpResponse serverHttpResponse = exchange.getResponse();
+                    serverHttpResponse.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+                    serverHttpResponse.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+                    return writeResponse(ResultEntity.fail("500", ""), exchange);
+
+                });
+    }
+
+    /**
+     * 数据写回前端
+     *
+     * @param resultEntity
+     * @param exchange
+     * @return
+     */
+    private Mono<Void> writeResponse(ResultEntity resultEntity, ServerWebExchange exchange) {
+        byte[] data = JSON.toJSONString(resultEntity).getBytes(StandardCharsets.UTF_8);
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(data);
+        return exchange.getResponse().writeWith(Mono.just(buffer));
+    }
+
+    ;
+
+}
+
+
