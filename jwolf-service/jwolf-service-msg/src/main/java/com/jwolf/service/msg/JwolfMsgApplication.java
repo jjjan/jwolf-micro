db46d00fec20addbddfbc0ba1ed0d805120e28cb
majun <523083921@qq.com>
2021-11-20 22:23:08 +0800

    kafka测试，注意docker-compose的环境变量- KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://192.168.154.140:9092  #！！！！向外部暴露的地址！！！,即client端连接需走这个地址，另调整server版本与springboot版本对应的2.6.2->2.6.0

--- jwolf-service/jwolf-service-msg/src/main/java/com/jwolf/service/msg/JwolfMsgApplication.java
+++ jwolf-service/jwolf-service-msg/src/main/java/com/jwolf/service/msg/JwolfMsgApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.jwolf.service")
 @EnableOpenApi //EnableSwagger2
---
+++ jwolf-service/jwolf-service-msg/src/main/java/com/jwolf/service/msg/kafka/ProducerConsumerTest.java
@@ -0,0 +1,52 @@
+package com.jwolf.service.msg.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@Component
+@Slf4j
+@EnableScheduling
+public class ProducerConsumerTest {
+    @Autowired
+    private KafkaTemplate<Integer, String> kafkaTemplate;
+
+
+    @Scheduled(cron = "0/5 * * * * ? ")
+    public void send() {
+        ListenableFuture future = kafkaTemplate.send("test5", "来自kafkaTemplate发送de消息");
+        future.addCallback(o -> System.out.println("发送成功"), e -> System.err.println("发送失败," + e.getCause()));
+    }
+    //同一个进程的两个xxx消费者不会共同消费
+    @KafkaListener(topics = "test5", groupId = "xxx")
+    public void getMsg1(String msg) {
+        System.out.println("xxx1" + msg);
+    }
+
+    @KafkaListener(topics = "test5", groupId = "xxx")
+    public void getMsg2(String msg) {
+        System.out.println("xxx2" + msg);
+    }
+
+    @KafkaListener(topics = "test5", groupId = "yyy")
+    public void getMsg3(String msg) {
+        System.out.println("yyy" + msg);
+    }
+
+    //配置 ack模式为手动提交才正常消费
+    @KafkaListener(topics = "test5", groupId = "zzz")
+    public void manualImmediateTest(ConsumerRecord<String, Object> record, Consumer consumer, Acknowledgment ack) {
+        log.info("zzz" + record.value());
+        log.info("consumer content = {}", consumer);
+        ack.acknowledge();
+
+    }
+}
