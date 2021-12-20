23c5930c9405200044bed9cdf8b9b0f4b3fd51db
j_maj <523083921@qq.com>
2021-12-20 22:52:10 +0800

    vue3 web前端项目 init and test

---
+++ jwolf-web-ui/README.md
@@ -0,0 +1,30 @@
+# jwolf-web-ui
+```
+1.cnpm i -g @vue/cli 或npm i -g @vue/cli,可以需要npm i -g @vue/cli-init
+2.vue create jwolf-web-ui,也可以使用IDE或vue ui启动web界面来创建项目
+3.进入package.json目录，npm run serve启动，可能报错缺一个module:npm i vue-loader-v16 -S
+
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ jwolf-web-ui/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld  msg="Welcome to Your Vue.js App"/>
+  <el-button style="font-size:xx-large;color: red" @click.stop="clickme">点我</el-button>
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      date:'2021-12'
+
+    }
+  },
+  methods:{
+    clickme(){
+      alert(new Date);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
